4277f1ae7cbd43293e8b89ea1f63d5616dcead1d
segerasimchik <segerasimchik@gmail.com>
2020-06-03 17:59:57 +0300

    Added: SG HTTP, internal IPs and some small fixes

--- example.tf
+++ example.tf
@@ -18,8 +18,9 @@ resource "aws_internet_gateway" "my_gw" {
       Name = "MyInternetGateway"
     }
 }
-resource "aws_route_table" "my" {
-    vpc_id = aws_vpc.my_vpc.id
+
+resource "aws_default_route_table" "my" {
+    default_route_table_id = aws_vpc.my_vpc.default_route_table_id
     route {
       cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.my_gw.id
@@ -31,7 +32,7 @@ resource "aws_route_table" "my" {
 
 resource "aws_main_route_table_association" "tab" {
     vpc_id = aws_vpc.my_vpc.id
-    route_table_id = aws_route_table.my.id
+    route_table_id = aws_default_route_table.my.id
 }
 
 ##### First subnet #####
@@ -51,14 +52,15 @@ resource "aws_subnet" "my_sub" {
 resource "aws_subnet" "my_sec_sub" {
     vpc_id = aws_vpc.my_vpc.id
     cidr_block = "10.1.20.0/24"
-    #map_public_ip_on_launch = true
     availability_zone = "eu-west-3b"
     tags = {
       Name = "From Terraform With Love"
     }
 }
 
-##### SG allow ssh ####
+########## Security Groups ##########
+
+##### SG allow ssh #####
 resource "aws_security_group" "ssh" {
     name = "Just SSH"
     vpc_id = aws_vpc.my_vpc.id
@@ -77,33 +79,54 @@ resource "aws_security_group" "ssh" {
     }
 }
 
+##### SG allow http #####
+resource "aws_security_group" "http" {
+    name = "HTTP"
+    vpc_id = aws_vpc.my_vpc.id
+
+    ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
 
 ########## End of network description ##########
 
-########## EC2_instance ##########
+########## EC2_instances ##########
 
-##### First instance #####
+##### Instance with publicIP #####
 
 resource "aws_instance" "host_1" {
     ami = "ami-08c757228751c5335"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.my_sub.id
-    security_groups = ["${aws_security_group.ssh.id}"]
+    private_ip = "10.1.10.100"
+    security_groups = ["${aws_security_group.ssh.id}",
+      "${aws_security_group.http.id}"]
     key_name = var.key_name
     tags = {
-      Name = "host_1"
+      Name = "Host_IFaced"
     }
-  }
+}
 
-##### Second instance #####
+##### Instance with just privateIP #####
 
 resource "aws_instance" "host_2" {
     ami = "ami-08c757228751c5335"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.my_sec_sub.id
+    private_ip = "10.1.20.200"
     security_groups = ["${aws_security_group.ssh.id}"]
     key_name = var.key_name
     tags = {
-      Name = "host_2"
+      Name = "Host_Internal"
     }
   }
